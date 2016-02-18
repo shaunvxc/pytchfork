56f71bed235a4e6f54b98455d22a966618c56577
shaunvxc <shaun@yipitdata.com>
2016-02-18 01:31:25 -0500

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@
 
 from setuptools import setup
 
+install_requires = [
+    "redis==2.10.3",
+    "multiprocessing_logging",
+]
+
 setup(
     name='pytchfork',
-    version='0.0.1',
+    version='0.0.2',
     description='multiprocessing decorator with queue management & redis features',
     author='Shaun Viguerie',
     author_email='shaunvig114@gmail',
     url='https://github.com/shaunvxc/pytchfork',
+    install_requires=install_requires,
+    license="MIT",
     packages=['pytchfork'],
     zip_safe=True
 )
