8de475366483185c33c9b63d379889d3841e75cd
shaunvxc <shaun@yipitdata.com>
2016-02-17 22:43:34 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ from setuptools import setup
 setup(
     name='pytchfork',
     version='0.0.1',
-    description='Little decorator for multiprocessing',
+    description='multiprocessing decorator with queue management & redis features',
     author='Shaun Viguerie',
     author_email='shaunvig114@gmail',
     url='https://github.com/shaunvxc/pytchfork',
     packages=['pytchfork'],
-    scripts=['bin/pytchfork'],
     zip_safe=True
 )
